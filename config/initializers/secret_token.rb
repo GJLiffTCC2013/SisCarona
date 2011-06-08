b8b6971ea5a397099bc149c0a1c4d174b3d0cd1d
Gabriel Alho <bielalho@gmail.com>
2011-06-08 11:18:43 -0300

    Estrutura Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaDeCarona::Application.config.secret_token = '3203f34a6e6aca9f81ff23f770c6a6d4c60affd5014a68e6fdb5d283783d9afd2c999e187060999fde13b6e1486454a6a31685bf4eb9fe15c6277c5d4647b6dc'
