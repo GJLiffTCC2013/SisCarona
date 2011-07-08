d453980b39b9a37811d427ca26b1038f4f9fb863
Gabriel Alho <bielalho@gmail.com>
2011-07-08 11:56:22 -0300

    Alteraçao dos models com validacoes

--- app/models/veiculo.rb
+++ app/models/veiculo.rb
@@ -1,2 +1,7 @@
 class Veiculo < ActiveRecord::Base
+  validates_presence_of :placa, :message => "O campo Placa deve ser preenchido"
+  validates_presence_of :modelo, :message => "O campo Modelo deve ser preenchido"
+  validates_presence_of :marca, :message => "O campo Marca deve ser preenchido"
+  validates_presence_of :cor, :message => "O campo Cor deve ser preenchido"
+  validates_presence_of :motorista_id, :message => "O campo Motorista deve ser preenchido"
 end
