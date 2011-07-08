4f53237fb5059eacbc482c42a02ad7b15b2995e8
Felipe Duarte <felipe.duarte55@yahoo.com.br>
2011-07-08 12:12:10 -0300

    Validacoes dos models

--- app/models/estado.rb
+++ app/models/estado.rb
@@ -1,3 +1,3 @@
 class Estado < ActiveRecord::Base
-	validates_precense_of :nome => "O preenchimento do campo nome e obrigatorio!"
+	validates_presence_of :nome => "O preenchimento do campo nome e obrigatorio!"
 end
--- app/models/reputacao_veiculo.rb
+++ app/models/reputacao_veiculo.rb
@@ -1,6 +1,6 @@
 class ReputacaoVeiculo < ActiveRecord::Base
-	validates_precense_of :carona_id => "O preenchimento do campo carona e obrigatorio!"
-	validates_precense_of :veiculo_id => "O preenchimento do campo veículo e obrigatorio!"
-	validates_precense_of :justificativa => "O preenchimento do campo justificativa e obrigatorio!"
-	validates_precense_of :ponto => "O preenchimento do campo ponto e obrigatorio!"
+	validates_presence_of :carona_id => "O preenchimento do campo carona e obrigatorio!"
+	validates_presence_of :veiculo_id => "O preenchimento do campo veículo e obrigatorio!"
+	validates_presence_of :justificativa => "O preenchimento do campo justificativa e obrigatorio!"
+	validates_presence_of :ponto => "O preenchimento do campo ponto e obrigatorio!"
 end
