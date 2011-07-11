d4a911a5a17d9fd586a277efcbf10be50482f19c
Lucas Manhes Sepulvida <lmsepulvida@yahoo.com.br>
2011-07-11 09:57:49 -0300

    Modificacoes nos models de reputacao de motorista e veiculo

--- app/models/reputacao_veiculo.rb
+++ app/models/reputacao_veiculo.rb
@@ -1,6 +1,9 @@
 class ReputacaoVeiculo < ActiveRecord::Base
 	validates_presence_of :carona_id, :message => "O preenchimento do campo carona e obrigatorio!"
-	validates_presence_of :veiculo_id, :message => "O preenchimento do campo veículo e obrigatorio!"
+	validates_presence_of :veiculo_id, :message => "O preenchimento do campo veiculo e obrigatorio!"
 	validates_presence_of :justificativa, :message => "O preenchimento do campo justificativa e obrigatorio!"
 	validates_presence_of :ponto, :message => "O preenchimento do campo ponto e obrigatorio!"
+	
+	belongs_to :veiculo
+	belongs_to :carona
 end
